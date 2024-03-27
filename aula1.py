a490a922210af50530e6aed619b902ee67609ee4
JUMALAUXEN <jacomasso.maria@escola.pr.gov.br>
2024-03-27 10:39:31 -0300

    subir aula1

---
+++ aula1.py
@@ -0,0 +1,11 @@
+import os
+
+#print("ola mundo cruel")
+print("O 3DS é a turma numero 1 do Sagrada")
+turma="3DS"
+pos=1
+print("O",turma,"é a turmanumero",pos,"do Sagrada")
+
+print(type(turma))
+print(type(pos))
+os.system("clear")
